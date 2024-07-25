93e74f71c906b12e17ec1a32d6fa250fddd08418
BlackC0ffee <BlackC0ffee@users.noreply.github.com>
2024-07-25 22:52:32 +0200

    First commit adding simple form

---
+++ NES/Cartridge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NES
+{
+    internal class Cartridge
+    {
+        private string catridgePath;
+
+        public Cartridge(string catridgePath)
+        {
+            this.catridgePath = catridgePath;
+        }
+
+        public string CatridgePath { get => catridgePath; set => catridgePath = value; }
+    }
+}
---
+++ NES/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NES
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == true)
+            {
+                Cartridge c = new Cartridge(ofd.FileName);
+                mainTextBlock.Text = c.CatridgePath;
+            }
+        }
+    }
+}
