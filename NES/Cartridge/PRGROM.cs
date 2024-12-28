2b8f730e0c12acc298004404ccd1595d8ebb0093
BlackC0ffee <BlackC0ffee@users.noreply.github.com>
2024-12-28 22:09:30 +0100

    Cleanup code and add PRGROM bank objects

---
+++ NES/Cartridge/PRGROM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NES.Cartridge {
+    internal class PRGROM {
+        private Byte[] _data;
+        public PRGROM(byte[] Data) {
+            //this._data = new Byte[16384];
+            this._data = Data;
+        }
+    }
+}
